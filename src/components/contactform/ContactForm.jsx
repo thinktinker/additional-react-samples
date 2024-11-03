d3e4d617903d1b36f7846849dae3935090914400
Leong Martin <leongmartin@Leongs-MacBook-Air.local>
2024-11-03 17:32:26 +0800

    update ContactForm to edit and delete contacts

--- src/components/contactform/ContactForm.jsx
+++ src/components/contactform/ContactForm.jsx
@@ -4,41 +4,106 @@ import { useState, useRef, useEffect } from 'react';
 function ContactForm() {
 
   const [fullName, setfullName] = useState({firstName:"", lastName:""});
+  const [editFullName, setEditFullName] = useState({firstName:"", lastName:""});
   const [contactsData, setContactsData] = useState([]);
   const inputRef = useRef(null);
   const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+  const [isEditDisabled, setIsEditDisabled] = useState(true);
 
+  /*
+  * Focus the input field when component mounts
+  */
   useEffect(() => {
-    // Focus the input field when component mounts
+    
     inputRef.current.focus();
   }, []);
 
+  /* 
+  * useEffect() hook here tracks the changes to state hook fullName
+  * Set the submit button only when firstName and lastName is not empty
+  * And when the edit button is disabled
+  */
   useEffect(() => {
-    if (fullName.firstName.trim() && fullName.lastName.trim()) {
+    if (fullName.firstName.trim() && fullName.lastName.trim() && isEditDisabled) {
       setIsBtnDisabled(false);
     } else {
       setIsBtnDisabled(true);
     }
-  }, [fullName])
 
+  }, [fullName]);
 
+  /* 
+  * Display the list of added contacts
+  */
   const contacts = contactsData.map(contact => 
+    <div className='contactItem'>
       <li key={contact.firstName + contact.lastName}>
         {contact.firstName} {contact.lastName}
       </li>
+      <button onClick={() => {
+        handleEdit(contact.firstName,contact.lastName);
+      }}>Edit</button>
+      <button onClick={() => {
+        handleDelete(contact.firstName,contact.lastName)
+      }}>Delete</button>
+    </div>
   );
-
-  function handleSubmit(event){
-    event.preventDefault()
-    setContactsData(prevContact=>([...prevContact, fullName]));
+  
+  /*
+   * Handle the event when the "Delete" button is selected
+   * Filters only the names that are not found from the selected for deletion
+   * Stores the filtered names back to state contactData 
+   */
+  function handleDelete(firstName, lastName){
+    const updatedContacts = contactsData.filter(
+      contact => contact.firstName+contact.lastName !== firstName+lastName
+    );
+    setContactsData(updatedContacts);
   }
 
+  /* 
+  * Handle the event when the "Update Contact" is selected (onClick())
+  * Updates the edited contact and re-populates state contactData
+  * map() updates the position of the array where the data matches (firstName, lastName)
+  */
+  function handleUpdate(){
+    const updatedContacts = contactsData.map(
+      contact => contact.firstName+contact.lastName === editFullName.firstName+editFullName.lastName ? fullName : contact
+    );
+    setContactsData(updatedContacts);
+    setfullName({firstName:"", lastName:""});
+    setEditFullName({firstName:"", lastName:""});
+    setIsEditDisabled(true);    
+  }
+  
+  /*
+   * Handle the event where the "Edit" button is selected (onClick()) 
+   * Setting the selected firstName and lastName to its respective inputs
+   */
+  function handleEdit(firstName, lastName) {
+    setfullName({firstName,lastName});
+    setIsEditDisabled(false);
+    setEditFullName({firstName,lastName});
+  }
+
+  /*
+  * Handle the event when the "Submit" button is selected (onSubmit())
+  * Inputs for firstName and lastName should exist before the 
+  */
+  function handleSubmit(event){
+    event.preventDefault();
+    setContactsData(prevContact=>([...prevContact, fullName]));
+    setfullName({firstName:"", lastName:""});
+  }
+
+  /*
+   * Handle the event when inputs firstName or lastName changes
+   * Ensure that the name attribute of each input is consistent with each key in state fullName
+   */
   function handleChange(event){
     const {name, value} = event.target;
-    setfullName(prevContact => ({...prevContact, [name]: value}));
+    setfullName(prevContact => ({...prevContact, [name]: value.trim()}));
   }
-
-  console.log(fullName);
 
   return (
     <>
@@ -62,9 +127,12 @@ function ContactForm() {
         <button disabled={isBtnDisabled}>
           Add Contact
         </button>
+        <button disabled={isEditDisabled} onClick={handleUpdate}>
+          Update Contact
+        </button>
       </form>
       <div className="contactbox">
-        <h5>Contact:</h5>
+        <h3>Contact:</h3>
         <ul>
           {contacts}
         </ul>
